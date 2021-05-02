688dc1d93924ddd5ea94cdb2d0afa67318767a4e
ald-ac <muriasaldair@hotmail.com>
2021-05-01 21:02:09 -0500

    Creando objetos UI y Seguro, protoype y cargando select del formulario

---
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -0,0 +1,32 @@
+//CONSTRUCTORES
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI() {}
+
+
+//Llena el select de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+
+          const selectYear = document.querySelector('#year');
+        
+          for (let i = max; i > min; i--) {
+              let option = document.createElement('option');
+              option.value = i;
+              option.textContent = i;
+              selectYear.appendChild(option);
+          }
+}
+
+//Instanciar UI
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); //Llenar select con los años
+})
